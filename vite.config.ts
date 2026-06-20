97f4c622cb5fdf1924247ce78219c5827c2a4eae
mst-mkt <mail@keito.dev>
2026-06-21 01:04:39 +0900

    feat: add document page with vite, scalar

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,51 @@ export default defineConfig({
   },
   run: {
     tasks: {
-      build: {
-        command: 'tsp compile ./src/',
-        output: ['dist/**'],
+      dev: {
+        // parallel (watch:tsp, watch:docs)
+        command: '',
+        dependsOn: ['watch:tsp', 'watch:docs'],
+        cache: false,
       },
-      watch: {
+      build: {
+        // build:tsp -> build:docs
+        command: '',
+        dependsOn: ['build:docs'],
+      },
+      check: {
+        // parallel (check:tsp)
+        command: '',
+        dependsOn: ['check:tsp'],
+      },
+      fix: {
+        // parallel (fix:tsp)
+        command: '',
+        dependsOn: ['fix:tsp'],
+        cache: false,
+      },
+
+      // dependency tasks
+      'build:tsp': {
+        command: 'tsp compile ./src/',
+        output: ['dist/openapi/**'],
+      },
+      'build:docs': {
+        command: 'vp build',
+        output: ['dist/docs/**'],
+        dependsOn: ['build:tsp'],
+      },
+      'watch:tsp': {
         command: 'tsp compile ./src/ --watch',
         cache: false,
       },
-      check: {
+      'watch:docs': {
+        command: 'vp dev',
+        cache: false,
+      },
+      'check:tsp': {
         command: 'tsp format -c ./src/',
       },
-      fix: {
+      'fix:tsp': {
         command: 'tsp format ./src/',
         cache: false,
       },
@@ -31,5 +64,13 @@ export default defineConfig({
     sortPackageJson: {
       sortScripts: false,
     },
+  },
+
+  // OpenAPI documentation configuration
+  root: 'docs',
+  publicDir: '../dist/openapi',
+  build: {
+    outDir: '../dist/docs',
+    emptyOutDir: true,
   },
 })
