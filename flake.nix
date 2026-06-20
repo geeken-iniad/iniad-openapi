225e32eceb8e5ebdf62a2bba379503b2744b4e54
mst-mkt <mail@keito.dev>
2026-06-20 19:23:39 +0900

    init flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs-slim_24
+              pkgs.pnpm
+            ];
+          };
+        }
+      );
+    };
+}
