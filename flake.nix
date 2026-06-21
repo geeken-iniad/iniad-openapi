770a5cec28f30a0fb5e8d9f17768c5db1fbdec1a
mst-mkt <mail@keito.dev>
2026-06-21 19:27:38 +0900

    chore: manage vite-plus with flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,14 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-vite-plus = {
+      url = "github:ryoppippi/nix-vite-plus";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, nix-vite-plus, ... }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
@@ -17,6 +23,7 @@
             packages = [
               pkgs.nodejs-slim_24
               pkgs.pnpm
+              nix-vite-plus.packages.${system}.default
             ];
           };
         }
